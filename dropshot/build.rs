d4a2306382cffa686e66ee511e1e4e0b95e6ab4a
Benjamin Naecker <ben@oxide.computer>
2022-12-21 22:38:02 +0000

    Add changelog notes and comments about build.rs file

--- dropshot/build.rs
+++ dropshot/build.rs
@@ -13,13 +13,19 @@
 // limitations under the License.
 //
 
+// NOTE: The `usdt` crate uses inline assembly, which prior to Rust 1.59 or 1.66
+// on macOS, required the use of nightly features. These crate-local features
+// are used to determine which toolchain is used to compile Dropshot, and then
+// include the correct `#![feature(...)]` directives on the basis of that.
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
+    // `feature(asm)` is stable.
     if version_check::is_min_version("1.59").unwrap_or(false) {
         println!("cargo:rustc-cfg=usdt_stable_asm");
     }
 
+    // `feature(asm_sym)` is stable, which is required on macOS.
     #[cfg(target_os = "macos")]
     if version_check::is_min_version("1.66").unwrap_or(false) {
         println!("cargo:rustc-cfg=usdt_stable_asm_sym");
